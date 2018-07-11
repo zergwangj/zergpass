53f6be269f567e2fc8bbe119091956bf1fcec3f8
zergwangj <zergwangj@163.com>
2018-07-11 14:06:20 +0800

    First commit files

---
+++ db/db.go
@@ -0,0 +1,163 @@
+// Copyright © 2018 zergwangj <zergwangj@163.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package db
+
+import (
+	"github.com/boltdb/bolt"
+	"fmt"
+)
+
+const dbFile = "zergpass.db"
+const passwordsBucket = "passwords"
+
+type DB struct {
+	Password	string
+	Db			*bolt.DB
+}
+
+func NewDB(password string) *DB {
+	db, err := bolt.Open(dbFile, 0600, nil)
+	if err != nil {
+		return nil
+	}
+
+	err = db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(passwordsBucket))
+		if b == nil {
+			fmt.Println("No existing passwords found. Creating a new one...")
+			_, err := tx.CreateBucket([]byte(passwordsBucket))
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		return nil
+	}
+
+	d := &DB {
+		Password: 	password,
+		Db:  		db,
+	}
+	return d
+}
+
+func (d *DB) Close() {
+	d.Db.Close()
+}
+
+func (d *DB) Add(entry *Entry) error {
+	err := d.Db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(passwordsBucket))
+		if b != nil {
+			return nil
+		}
+		return fmt.Errorf("Bucket passwords is not found")
+	})
+	if err != nil {
+		return err
+	}
+
+	err = d.Db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(passwordsBucket))
+		if b != nil {
+			data, err := entry.Serialize()
+			if err != nil {
+				return err
+			}
+			err = b.Put([]byte(entry.Title), data)
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (d *DB) Delete(title string) error {
+	err := d.Db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(passwordsBucket))
+		if b != nil {
+			data := b.Get([]byte(title))
+			if data == nil {
+				return fmt.Errorf("Entry: %s not found", title)
+			}
+			return nil
+		}
+		return fmt.Errorf("Bucket passwords is not found")
+	})
+	if err != nil {
+		return err
+	}
+
+	err = d.Db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(passwordsBucket))
+		if b != nil {
+			err = b.Delete([]byte(title))
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (d *DB) Edit(entry *Entry) error {
+	err := d.Db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(passwordsBucket))
+		if b != nil {
+			data := b.Get([]byte(entry.Title))
+			if data == nil {
+				return fmt.Errorf("Entry: %s not found", entry.Title)
+			}
+			return nil
+		}
+		return fmt.Errorf("Bucket passwords is not found")
+	})
+	if err != nil {
+		return err
+	}
+
+	err = d.Db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(passwordsBucket))
+		if b != nil {
+			data, err := entry.Serialize()
+			if err != nil {
+				return err
+			}
+			err = b.Put([]byte(entry.Title), data)
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
